f789b1bfc72a1e5cf02ee1d15d91cf7197d0fce9
Zeleczek-kodowniczek <jakub.zelek.111@gmail.com>
2026-03-01 16:22:39 +0100

    alg 2-4

--- prime_path_algorithm.py
+++ prime_path_algorithm.py
@@ -12,7 +12,10 @@ class Path:
         return Path(self.path + other, set(), False, False)
 
     def __getitem__(self, index):
-        return self.path[index]
+        result = self.path[index]
+        if isinstance(index, slice):
+            return Path(result, set(), self.extended, self.is_cycle)
+        return result
 
     def __len__(self):
         return len(self.path)
@@ -25,6 +28,18 @@ class Path:
 
     def __repr__(self):
         return str(self.path)
+    
+    def __hash__(self):
+        return hash(tuple(self.path))
+    
+    def __eq__(self, other):
+        return "-".join(self.path) == "-".join(other.path)
+
+    def index(self, value):
+        return self.path.index(value)
+    
+    def replace(self, index, path: Path):
+        return Path 
 
 
 def algorithm_1(G: dict, s: str):
@@ -85,6 +100,72 @@ def algorithm_1(G: dict, s: str):
     return paths_ended_in_v
 
 
+def get_scc_internal_prime_paths(scc: dict):
+    scc_prime_paths = set()
+    for v in scc.keys():
+        paths_ended_in_v = algorithm_1(scc, v)
+        for values in paths_ended_in_v.values():
+            scc_prime_paths |= set(values)
+    return scc_prime_paths
+
+
+def algorithm_2(scc_internal_prime_paths: set[Path], v_en: str, v_ex: str):
+    entry_exit_paths = set()
+    if v_en == v_ex:
+        return v_en
+    
+    for path in scc_internal_prime_paths:
+        if v_en in path and v_ex in path:
+            v_en_index = path.index(v_en)
+            v_ex_index = path.index(v_ex)
+            
+            entry_exit_paths.add(path[v_en_index:v_ex_index + 1])
+    
+    copy_of_entry_exit_paths = set(entry_exit_paths)
+
+    for p in copy_of_entry_exit_paths:
+        for k in copy_of_entry_exit_paths:
+            if p != k and k < p:
+                if k in entry_exit_paths:
+                    entry_exit_paths.remove(k)
+    return entry_exit_paths
+
+
+def algorithm_3(scc_internal_prime_paths: set[Path], v_ex: str):
+    exit_paths = set()
+    for path in scc_internal_prime_paths:
+        if v_ex in path and not (path.is_cycle and path[0] == v_ex):
+            v_ex_index = path.index(v_ex)
+            exit_paths.add(path[:v_ex_index + 1])
+    
+    copy_of_exit_paths = set(exit_paths)
+
+    for p in copy_of_exit_paths:
+        for k in copy_of_exit_paths:
+            if p != k and k < p:
+                if k in exit_paths:
+                    exit_paths.remove(k)
+    return exit_paths
+
+def algorithm_4(scc_internal_prime_paths: set[Path], v_en: str):
+    entry_paths = set()
+    for path in scc_internal_prime_paths:
+        if v_en in path and not (path.is_cycle and path[0] == v_en):
+            v_en_index = path.index(v_en)
+            entry_paths.add(path[v_en_index:])
+    
+    copy_of_exit_paths = set(entry_paths)
+    
+    for p in copy_of_exit_paths:
+        for k in copy_of_exit_paths:
+            if p != k and k < p:
+                if k in entry_paths:
+                    entry_paths.remove(k)
+    return entry_paths
+
+
+def algorithm_5(scc_entry_exit_paths: set[Path]):
+
 def generate_diamond(k: int, start_label="B0") -> dict:
    
     graph = {}
@@ -112,12 +193,12 @@ def generate_diamond(k: int, start_label="B0") -> dict:
 
 
 
-for i in range(1, 40):
-    s = time.time()
-    d = generate_diamond(i)
-    algorithm_1(d, "B0")
-    t = time.time()
-    print(i, 2**i, t-s)
+# for i in range(1, 40):
+#     s = time.time()
+#     d = generate_diamond(i)
+#     algorithm_1(d, "B0")
+#     t = time.time()
+#     print(i, 2**i, t-s)
 
 
 # from diblob.algorithms import PrimePathGenerator
@@ -138,3 +219,17 @@ for i in range(1, 40):
 #         pass
 #     t = time.time()
 #     print(i, 2**i, t-s)
+
+
+scc = {"1": ["3"],
+       "2": ["1"],
+       "3": ["2", "4"],
+       "4": ["6"],
+       "6": ["5", "1"],
+       "5": ["4"]}
+
+internal_prime_paths = get_scc_internal_prime_paths(scc)
+
+print(algorithm_2(internal_prime_paths, v_en="2", v_ex="5"))
+print(algorithm_3(internal_prime_paths, v_ex="5"))
+print(algorithm_4(internal_prime_paths, v_en="5"))
